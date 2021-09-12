16543e2883fc0b2da7e100b3965109386d284b13
Jonny <faane784@gmail.com>
2021-09-12 08:14:10 -0700

    Pushing ssmall changes

--- layers/dense.py
+++ layers/dense.py
@@ -1,14 +1,22 @@
 from layers import Layer
+from activations import Activation
 import numpy.typing as npt
 import numpy as np
 
 
 class Dense(Layer):
-    def __init__(self, units, trainable=True) -> None:
+    def __init__(self, units: int, activation: Activation = None, trainable: bool = True) -> None:
         super().__init__(trainable=trainable)
         self.units = units
+        self.activation = activation
 
-    def build(self, input_shape):
+    def build(self, input_shape: tuple):
+        """
+        Initialize the correct shapes. Create the bias and weight matricies.
+
+        Parameters:
+        input_shape: a tuple of the dimensions.
+        """
         self.input_shape = input_shape
         self.output_shape = (self.units, input_shape[-1])
         self.weights = np.random.randn(self.output_shape)
@@ -16,7 +24,21 @@ class Dense(Layer):
         self.__built = True
 
     def call(self, inputs) -> npt.ArrayLike:
-        a = inputs
-        for b, w in zip(self.weights, self.biases):
-            a = np.dot(w, a) + b
+        """
+        Do a feedforward pass of the layer.
+
+        If activation is `None`, the activation is linear (np.dot(weights, inputs) + b).
+
+        Paramters:
+
+        inputs: An array-like of inputs.
+
+        Return:
+        The activation of the layer.
+        """
+        if not self.__built:
+            self.build(inputs.shape)
+        a = np.dot(self.weights, inputs) + self.biases
+        if self.activation is not None:
+            a = self.activation(a)
         return a
